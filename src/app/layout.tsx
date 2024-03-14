5972bce3c8d1852e6877457ad51b52cd9470f72b
khanhcutedz <nguyenvietkhanh24112001@gmail.com>
2024-03-14 18:49:55 +0700

    add yarn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,12 +12,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="h-full">
+      <body className={cn("realative h-full font-sans antialiased", inter.className)}>{children}</body>
     </html>
   );
 }
