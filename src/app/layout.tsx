4fe29674814161bd3c98d7758adc6428678ad9f7
khanhcutedz <nguyenvietkhanh24112001@gmail.com>
2024-03-15 15:59:56 +0700

    Creating our landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn("realative h-full font-sans antialiased", inter.className)}>{children}</body>
+      <body 
+        className={cn(
+          "realative h-full font-sans antialiased", 
+          inter.className
+      )}>
+      <main className="relative flex flex-col min-h-screen">
+        <div className="flex-grow flex-1">{children}</div>
+      </main>
+      </body>
     </html>
   );
 }
